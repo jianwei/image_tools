918d03dbe3ddcc934e304c6d0b163b1b30034c5c
Carlos  chen <ccjjww1222@gmail.com>
2022-10-12 16:41:08 +0800

    Initial commit

---
+++ face.py
@@ -0,0 +1,25 @@
+# coding=utf-8
+import cv2
+
+#创建新的cam对象
+cap = cv2.VideoCapture(0)
+# cap = cv2.VideoCapture('video.mp4')
+#初始化人脸识别器（默认的人脸haar级联）
+face_cascade = cv2.CascadeClassifier(r'./haarcascade_frontalface_default.xml')
+
+while True:
+    # 从摄像头读取图像
+    _, image = cap.read()
+    # 转换为灰度
+    image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # 检测图像中的所有人脸
+    faces = face_cascade.detectMultiScale(image_gray, 1.3, 5)
+    # 为每个人脸绘制一个蓝色矩形
+    for x, y, width, height in faces:
+        cv2.rectangle(image, (x, y), (x + width, y + height), color=(255, 0, 0), thickness=2)
+    cv2.imshow("image", image)
+    if cv2.waitKey(1) == ord("q"):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
---
+++ track.py
@@ -0,0 +1,28 @@
+# conda install opencv-python==4.4.0.46
+# conda install opencv-contrib-python==4.4.0.46
+# conda install opencv-contrib-python-headless==4.4.0.46
+
+import cv2
+print(cv2.__version__)
+
+tracker = cv2.legacy.TrackerCSRT_create()
+cap = cv2.VideoCapture("2.mp4") 
+# cap = cv2.VideoCapture(0)
+ret,frame = cap.read()
+bbox = cv2.selectROI("Frame",frame,fromCenter=False,showCrosshair=True)
+print("bbox:",bbox)
+tracker.init(frame,bbox)
+while (True):
+    ret,frame = cap.read()
+    ok,box = tracker.update(frame)
+    if(ok):
+        (x,y,w,h) = [int(v) for v in box]
+        cv2.rectangle(frame,pt1=(x,y),pt2=(x+w,y+h),color=(0,255,0),thickness=2)
+    cv2.imshow("Frame",frame)
+    if(cv2.waitKey(50)==ord("q")):
+        break
+cap.release()
+cv2.destroyAllWindows()
+
+
+    
